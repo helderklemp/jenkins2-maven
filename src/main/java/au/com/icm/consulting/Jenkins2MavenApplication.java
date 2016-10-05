5741c583431c6c2edf07f712126aacbcdb4a44a0
Helder Klemp <hklemp@icm-consulting.com.au>
2016-10-05 20:45:21 +1100

    Initial commit

---
+++ src/main/java/au/com/icm/consulting/Jenkins2MavenApplication.java
@@ -0,0 +1,12 @@
+package au.com.icm.consulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins2MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins2MavenApplication.class, args);
+	}
+}
---
+++ src/test/java/au/com/icm/consulting/Jenkins2MavenApplicationTests.java
@@ -0,0 +1,16 @@
+package au.com.icm.consulting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jenkins2MavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
